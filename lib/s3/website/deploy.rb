515d138adef20087084234b1983fcae910b4b659
hogelog <konbu.komuro@gmail.com>
2018-05-17 22:41:01 +0900

    Init repo

---
+++ lib/s3/website/deploy.rb
@@ -0,0 +1,9 @@
+require "s3/website/deploy/version"
+
+module S3
+  module Website
+    module Deploy
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/s3/website/deploy/version.rb
@@ -0,0 +1,7 @@
+module S3
+  module Website
+    module Deploy
+      VERSION = "0.1.0"
+    end
+  end
+end
