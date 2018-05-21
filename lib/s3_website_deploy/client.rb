d7d9bbe6303797633d12b10e551d8d20816ce8ee
hogelog <konbu.komuro@gmail.com>
2018-05-22 00:13:18 +0900

    Add delta deploy feature

--- lib/s3_website_deploy/client.rb
+++ lib/s3_website_deploy/client.rb
@@ -5,8 +5,10 @@ require "aws-sdk-s3"
 
 module S3WebsiteDeploy
   class Client
-    LocalFile = Struct.new(:path, :local_path)
-    S3File = Struct.new(:path, :s3_uri)
+    CONTENT_MD5_KEY = "content-md5"
+
+    LocalFile = Struct.new(:path, :local_path, :content_md5)
+    S3File = Struct.new(:path, :s3_uri, :etag)
 
     def initialize(source:, region:, bucket:, prefix:, logger: Logger.new(STDOUT))
       @source = Pathname.new(source)
@@ -37,15 +39,27 @@ module S3WebsiteDeploy
 
     def deploy(file_stats)
       file_stats.each do |path, stat|
-        if stat[:local]
-          deploy_local_file(stat[:local])
+        local_file = stat[:local]
+        remote_file = stat[:remote]
+        if local_file
+          if !remote_file
+            @logger.info("Creating: #{local_file.path}")
+            deploy_local_file(local_file)
+          elsif local_file.content_md5 != remote_file.etag
+            @logger.info("Updating: #{local_file.path}")
+            deploy_local_file(local_file)
+          else
+            @logger.info("Skip: #{local_file.path}")
+          end
+        elsif remote_file
+          @logger.info("Deleting: #{remote_file.path}")
+          delete_remote_file(remote_file)
         end
       end
     end
 
     def deploy_local_file(local_file)
       key = "#{@prefix}#{local_file.path}"
-      @logger.info("Create: #{local_file.local_path} -> s3://#{@bucket}/#{key}")
       File.open(local_file.local_path, "rb") do |file|
         s3.put_object(
           body: file,
@@ -55,9 +69,19 @@ module S3WebsiteDeploy
       end
     end
 
+    def delete_remote_file(remote_file)
+      key = "#{@prefix}#{remote_file.path}"
+      s3.delete_object(
+        bucket: @bucket,
+        key: key,
+      )
+    end
+
     def source_files
       Pathname.glob(@source.join("**")).map do |pathname|
-        LocalFile.new(pathname.relative_path_from(@source).to_s, pathname.to_s)
+        path = pathname.to_s
+        content_md5 = Digest::MD5.hexdigest(File.read(path))
+        LocalFile.new(pathname.relative_path_from(@source).to_s, path, content_md5)
       end
     end
 
@@ -74,7 +98,7 @@ module S3WebsiteDeploy
         res.contents.each do |content|
           pathname = Pathname.new(content.key)
           uri = "s3://#{@bucket}/#{content.key}"
-          files << S3File.new(pathname.relative_path_from(prefix_pathname).to_s, uri)
+          files << S3File.new(pathname.relative_path_from(prefix_pathname).to_s, uri, JSON.parse(content.etag))
         end
         next_token = res.next_continuation_token
         break unless next_token
